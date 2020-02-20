cc18616f2c1551bda85826e218fb52507cdf66c8
RomanAchilov <dakaktak95@gmail.com>
2020-02-20 17:48:41 +0300

    EPMDJ-2283: Implement breadcrumbs functionality
    Issue: EPMDJ-2283

--- src/modules/breadcrumbs/breadcrumbs.tsx
+++ src/modules/breadcrumbs/breadcrumbs.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { BEM } from '@redneckz/react-bem-helper';
 import {
-  useLocation, matchPath, Link,
+  useLocation, matchPath, Link, useHistory,
 } from 'react-router-dom';
 
 import styles from './breadcrumbs.module.scss';
@@ -9,8 +9,6 @@ import styles from './breadcrumbs.module.scss';
 interface Props {
   className?: string;
 }
-
-const breadcrumbs = BEM(styles);
 
 type MatchType = {
   agentId: string;
@@ -23,6 +21,8 @@ type MatchType = {
   page: string;
   scopeId: string;
 };
+
+const breadcrumbs = BEM(styles);
 
 export const Breadcrumbs = breadcrumbs(({ className }: Props) => {
   const { pathname } = useLocation();
@@ -50,7 +50,9 @@ export const Breadcrumbs = breadcrumbs(({ className }: Props) => {
     ],
     exact: true,
   }) || {};
-
+  const { location: { state: path } } = useHistory();
+  const getAgentDashboardPathname = () =>
+    (pathname.includes('dashboard') ? pathname : `/full-page/${agentId}/${buildVersion}/${pluginId}/dashboard`);
   return (
     <div className={className}>
       {(agentId || serviceGroupId) && <Crumb to="/">Agents</Crumb>}
@@ -58,16 +60,31 @@ export const Breadcrumbs = breadcrumbs(({ className }: Props) => {
         <Crumb to={`/agents/${agentType}/${agentId}/settings`}>{agentType === 'service-group' ? 'Service Group' : 'Agent'} Settings</Crumb>}
       {registrationType === 'registration' && <Crumb to="/">Agent registration</Crumb>}
       {registrationType === 'bulk-registration' && <Crumb to="/">Agents registration</Crumb>}
-      {buildVersion && pluginId === 'dashboard' && <Crumb to={`/full-page/${agentId}/${buildVersion}/dashboard`}>Agent: Dashboard</Crumb>}
+      {buildVersion && pluginId === 'dashboard'
+        && <Crumb to={path?.agentDashboardPathname}>Agent: Dashboard</Crumb>}
       {buildVersion && pluginId !== 'dashboard'
-        && <Crumb to={`/full-page/${agentId}/${buildVersion}/${pluginId}/dashboard`}>Agent: Test2Code</Crumb>}
+        && (
+          <Crumb to={path?.agentDashboardPathname || getAgentDashboardPathname()}>
+          Agent:
+            {path?.agentDashboardPathname.includes('test2code') || getAgentDashboardPathname().includes('test2code')
+              ? ' Test2Code' : ' Dashboard'}
+          </Crumb>
+        )}
       {serviceGroupId &&
         pluginId === 'service-group-dashboard' &&
           <Crumb to={`/service-group-full-page/${serviceGroupId}/serice-group-dashboard`}>Service Group: Dashboard</Crumb>}
-      {serviceGroupId &&
-        pluginId !== 'service-group-dashboard'
-          && <Crumb to={`/service-group-full-page/${serviceGroupId}/serice-group-dashboard`}>Service Group: Test2Code</Crumb>}
-      {buildVersion && <Crumb to={`/full-page/${agentId}/build-list`}>All builds</Crumb>}
+      {serviceGroupId && pluginId !== 'service-group-dashboard'
+        && <Crumb to={`/service-group-full-page/${serviceGroupId}/serice-group-dashboard`}>Service Group: Test2Code</Crumb>}
+      {buildVersion
+        && (
+          <Crumb to={{
+            pathname: `/full-page/${agentId}/build-list`,
+            state: { agentDashboardPathname: getAgentDashboardPathname() },
+          }}
+          >
+            All builds
+          </Crumb>
+        )}
       {buildVersion && buildVersion !== 'build-list'
         && <Crumb to={`/full-page/${agentId}/${buildVersion}/${pluginId}/dashboard`}>{buildVersion}</Crumb>}
       {page === 'scopes' && <Crumb to={`/full-page/${agentId}/${buildVersion}/${pluginId}/scopes`}>Scopes</Crumb>}
