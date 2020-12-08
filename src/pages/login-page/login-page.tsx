630dec3d21c109ebada934cb6f1ba0f40ec7ad76
Pavel Sokolov <49731235+SMPlay@users.noreply.github.com>
2020-12-08 15:07:05 +0300

    EPMDJ-5611: Update login page
    Issue: EPMDJ-5611

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { BEM } from '@redneckz/react-bem-helper';
 import { useHistory } from 'react-router-dom';
-import axios, { AxiosError } from 'axios';
-import { Inputs, Button } from '@drill4j/ui-kit';
+import axios from 'axios';
+import {
+  Inputs, Button, GeneralAlerts, Panel,
+} from '@drill4j/ui-kit';
 
 import { LoginLayout } from 'layouts';
 import { defaultAdminSocket, defaultTest2CodePluginSocket, getSocketUrl } from 'common/connection';
 import { TOKEN_HEADER, TOKEN_KEY } from 'common/constants';
-import { ErrorPanel } from './error-panel';
 
 import styles from './login-page.module.scss';
 
@@ -18,53 +19,55 @@ interface Props {
 const loginPage = BEM(styles);
 
 export const LoginPage = loginPage(({ className }: Props) => {
-  const [error, setError] = React.useState<AxiosError | null>(null);
+  const [error, setError] = React.useState<string | null>(null);
   const { push } = useHistory();
 
   async function handleLogin() {
-    await axios
-      .post('/login')
-      .then((response) => {
-        const authToken = response.headers[TOKEN_HEADER.toLowerCase()];
-        if (authToken) {
-          localStorage.setItem(TOKEN_KEY, authToken);
-        }
-        defaultAdminSocket.reconnect(getSocketUrl('drill-admin-socket'));
-        defaultTest2CodePluginSocket.reconnect(getSocketUrl('/plugins/test2code'));
-        push('/');
-      })
-      .catch((err: AxiosError) => setError(err));
+    try {
+      const response = await axios.post('/login');
+      const authToken = response.headers[TOKEN_HEADER.toLowerCase()];
+      if (authToken) {
+        localStorage.setItem(TOKEN_KEY, authToken);
+      }
+      defaultAdminSocket.reconnect(getSocketUrl('drill-admin-socket'));
+      defaultTest2CodePluginSocket.reconnect(getSocketUrl('/plugins/test2code'));
+      push('/');
+    } catch ({ response: { data: { message = '' } = {} } = {} }) {
+      setError(message || 'There was some issue with an authentication. Please try again later.');
+    }
   }
 
   return (
     <LoginLayout>
       <div className={className}>
-        <Logo />
-        <Title>Welcome to Drill4J</Title>
-        <SubTitle>Click &quot;Continue as a guest&quot; to entry Admin Panel with admin privilege</SubTitle>
-        {error && <Error>{`${error.message}`}</Error>}
-        <SignInForm>
-          <Inputs.Text placeholder="User ID" disabled />
-          <Inputs.Text placeholder="Password" disabled />
-        </SignInForm>
-        <LoginPageButtonWrapepr type="primary" size="large" disabled>
-          Sign in
-        </LoginPageButtonWrapepr>
-        <ForgotPasswordLink>Forgot your password?</ForgotPasswordLink>
-        <LoginPageButtonWrapepr type="secondary" size="large" onClick={handleLogin}>
-          Continue as a guest (read only)
-        </LoginPageButtonWrapepr>
+        <Content align="center" direction="column">
+          <Title>Welcome to Drill4J</Title>
+          <SubTitle>Click &quot;Continue as a guest&quot; to entry Admin Panel with admin privilege</SubTitle>
+          {error && <Error type="ERROR">{`${error}`}</Error>}
+          <SignInForm>
+            <Inputs.Text placeholder="User ID" disabled />
+            <Inputs.Text placeholder="Password" disabled />
+          </SignInForm>
+          <LoginButton type="primary" size="large" disabled>
+            Sign in
+          </LoginButton>
+          <ForgotPasswordLink>Forgot your password?</ForgotPasswordLink>
+          <LoginAsGuestButton type="secondary" size="large" onClick={handleLogin}>
+            Continue as a guest (read only)
+          </LoginAsGuestButton>
+        </Content>
         <Copyright>{`© ${new Date().getFullYear()} Drill4J. All rights reserved.`}</Copyright>
       </div>
     </LoginLayout>
   );
 });
 
-const Logo = loginPage.logo('div');
+const Content = loginPage.content(Panel);
 const Title = loginPage.title('div');
 const SubTitle = loginPage.subTitle('div');
-const Error = loginPage.error(ErrorPanel);
+const Error = loginPage.error(GeneralAlerts);
 const SignInForm = loginPage.signInForm('div');
 const ForgotPasswordLink = loginPage.forgotPassword('div');
-const LoginPageButtonWrapepr = loginPage.loginPageButtonWrapepr(Button);
+const LoginButton = loginPage.loginButton(Button);
+const LoginAsGuestButton = loginPage.loginAsGuestButton(Button);
 const Copyright = loginPage.copyright('div');
