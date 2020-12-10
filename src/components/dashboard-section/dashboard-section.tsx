6e1a9743f8f126a848d2c18efb77241fffdb305d
Pavel Sokolov <49731235+SMPlay@users.noreply.github.com>
2020-12-10 09:46:18 +0300

    EPMDJ-5623: Add data-test attributes to dashboard
    Issue: EPMDJ-5623

--- src/components/dashboard-section/dashboard-section.tsx
+++ src/components/dashboard-section/dashboard-section.tsx
@@ -1,26 +1,28 @@
 import * as React from 'react';
 import { BEM } from '@redneckz/react-bem-helper';
 
+import { spacesToDashes } from 'utils';
+
 import styles from './dashboard-section.module.scss';
 
 interface Props {
   className?: string;
   graph?: React.ReactNode | React.ReactNode[];
   info?: React.ReactNode;
-  label?: React.ReactNode;
+  label?: string;
   additionalInfo?: React.ReactNode;
 }
 
 const dashboardSection = BEM(styles);
 
 export const DashboardSection = dashboardSection(({
-  className, graph, info, label, additionalInfo,
+  className, graph, info, label = '', additionalInfo,
 }: Props) => (
   <div className={className}>
     <Info>
       <Label>{label}</Label>
-      <MainInfo>{info}</MainInfo>
-      <AdditionalInfo>{additionalInfo}</AdditionalInfo>
+      <MainInfo data-test={`dashboard:${spacesToDashes(label)}:main-info`}>{info}</MainInfo>
+      <AdditionalInfo data-test={`dashboard:${spacesToDashes(label)}:additional-info`}>{additionalInfo}</AdditionalInfo>
     </Info>
     <Graph>{graph}</Graph>
   </div>
