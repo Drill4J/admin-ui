accca08c76a45b1809235e11d87f5d24cb581ea6
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-01-14 10:27:21 +0300

    EPMDJ-5843: Integrate bootstrap
    Issue: EPMDJ-5843

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,8 @@ import { PageSwitcher } from './routes';
 import { NotificationManager } from './notification-manager';
 import { configureAxios } from './common/connection';
 
+import './app.scss';
+
 if (process.env.REACT_APP_ENV === 'development') {
   devirt();
 }
