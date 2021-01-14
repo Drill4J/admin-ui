bfbc2811216d88765b00db58682bb7360a7f17cd
RomanAchilov <dakaktak95@gmail.com>
2021-01-14 13:58:48 +0300

    EPMDJ-5858: Correct formatting of time values
    Issue: EPMDJ-5858

--- src/utils/get-duration.ts
+++ src/utils/get-duration.ts
@@ -4,9 +4,9 @@ export const getDuration = (value: number): { hours: string, minutes: string, se
       hours: '00', minutes: '00', seconds: '00', isLessThenOneSecond: false,
     };
   }
-  const seconds = (`0${Math.floor(value / 1000) % 60}`).slice(-2);
-  const minutes = (`0${Math.floor(value / 60000) % 60}`).slice(-2);
-  const hours = (`0${Math.floor(value / 3600000)}`).slice(-2);
+  const seconds = (`0${Math.round(value / 1000) % 60}`).slice(-2);
+  const minutes = (`0${Math.round(value / 60000) % 60}`).slice(-2);
+  const hours = (`0${Math.round(value / 3600000)}`).slice(-2);
   const isLessThenOneSecond = value > 0 && seconds === '00' && minutes === '00' && hours === '00';
 
   return {
