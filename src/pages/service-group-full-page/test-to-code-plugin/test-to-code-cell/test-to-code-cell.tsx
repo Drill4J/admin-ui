60605a086741ae604043baaf46c89bd58ac1e86f
RomanAchilov <dakaktak95@gmail.com>
2021-03-26 12:22:44 +0300

    feat: linit click area

--- src/pages/service-group-full-page/test-to-code-plugin/test-to-code-cell/test-to-code-cell.tsx
+++ src/pages/service-group-full-page/test-to-code-plugin/test-to-code-cell/test-to-code-cell.tsx
@@ -23,7 +23,7 @@ interface Props {
 }
 
 const Value = styled.span(({ clickable }: {clickable: boolean}) => [
-  tw`flex items-center text-20 text-monochrome-black`,
+  tw`inline-flex items-center text-20 text-monochrome-black`,
   clickable && tw`cursor-pointer hover:text-blue-default active:text-blue-shade`,
 ]);
 
