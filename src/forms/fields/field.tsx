746c4a0c5363962095e75432c14bcddeb2984435
RomanAchilov <dakaktak95@gmail.com>
2021-04-12 23:21:01 +0300

    feat: add trimEnd after onBlur

--- src/forms/fields/field.tsx
+++ src/forms/fields/field.tsx
@@ -39,7 +39,8 @@ export const field = <T, >(Input: React.ElementType) => (props: FieldRenderProps
         {...input}
         {...rest}
         error={isError}
-        onChange={input.type === 'checkbox' ? input.onChange : (event: any) => handleOnChange(input, event)}
+        onBlur={(event: React.ChangeEvent<HTMLInputElement>) => input.onChange({ target: { value: event.target.value.trimEnd() } })}
+        onChange={input.type === 'checkbox' ? input.onChange : (event: React.ChangeEvent<HTMLInputElement>) => handleOnChange(input, event)}
       />
       {isError && <ErrorMessage>{meta.error || meta.submitError}</ErrorMessage>}
     </>
