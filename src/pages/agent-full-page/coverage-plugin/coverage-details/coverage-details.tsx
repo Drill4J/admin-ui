dde5beb9d0e23a09769091e1ce5f27dc73b43a6e
RomanAchilov <dakaktak95@gmail.com>
2021-05-06 16:15:59 +0300

    feat: delete legacy coverage-details table

--- src/pages/agent-full-page/coverage-plugin/coverage-details/coverage-details.tsx
+++ src/pages/agent-full-page/coverage-plugin/coverage-details/coverage-details.tsx
@@ -24,7 +24,7 @@ import { ClassCoverage } from 'types/class-coverage';
 import { FilterList } from 'types/filter-list';
 import { useVisibleElementsCount, useBuildVersion } from 'hooks';
 import {
-  Cells, SearchPanel, Stub, Table as ReTable, TR,
+  Cells, SearchPanel, Table, TR,
 } from 'components';
 import {
   useTableActionsState, useTableActionsDispatch, setSearch,
@@ -32,7 +32,6 @@ import {
 import { Package } from 'types/package';
 import { NameCell } from './name-cell';
 import { AssociatedTestModal } from './associated-test-modal';
-import { ExpandableTable, Column } from './table';
 import { CoverageCell } from './coverage-cell';
 import { CellProps } from './table/table-types';
 
@@ -56,14 +55,7 @@ export const CoverageDetails = ({
   const ref = useRef<HTMLDivElement>(null);
   const visibleElementsCount = useVisibleElementsCount(ref, 10, 10);
   const [searchQuery] = search;
-  const expandedColumns = [
-    <Column
-      name="coverage"
-      Cell={({ value = 0 }) => <CoverageCell value={value as number} showCoverageIcon={showCoverageIcon} />}
-    />,
-    <Column name="totalMethodsCount" testContext="total-methods-count" />,
-    <Column name="coveredMethodsCount" testContext="covered-methods-count" />,
-  ];
+
   const {
     buildVersion, agentId, pluginId, scopeId, tab,
   } = useParams<{ agentId?: string; pluginId?: string; buildVersion?: string; scopeId?: string; tab: string; }>();
@@ -229,107 +221,7 @@ export const CoverageDetails = ({
             Displaying {coverageByPackages.slice(0, visibleElementsCount).length} of {totalCount} packages
           </SearchPanel>
         </div>
-        <ExpandableTable
-          data={coverageByPackages.slice(0, visibleElementsCount)}
-          idKey="name"
-          classesTopicPrefix={classesTopicPrefix}
-          tableContentStub={coverageByPackages.length === 0 && (
-            <Stub
-              icon={<Icons.Package height={104} width={107} />}
-              title="No results found"
-              message="Try adjusting your search or filter to find what you are looking for."
-            />
-          )}
-          expandedColumns={[
-            <Column
-              name="name"
-              Cell={(({ item: { name = '' } = {} }: CellProps<unknown, { name?: string }>) => (
-                <NameCell type="primary" icon={<Icons.Class width={16} height={16} />} value={name} testContext="class" />
-              ))}
-              align="start"
-            />,
-            ...expandedColumns,
-            <Column
-              name="assocTestsCount"
-              label="Associated tests"
-              Cell={({
-                value = '',
-                item: { id = '' } = {},
-              }: CellProps<string, { id?: string; assocTestsCount?: number }>) => (
-                <Cells.Clickable
-                  data-test="coverage-details:associated-tests-count"
-                  disabled={!value}
-                >
-                  {value ? <Link to={getModalLink(id, 2)}>{value}</Link> : 'n/a'}
-                </Cells.Clickable>
-              )}
-            />,
-          ]}
-          secondLevelExpand={[
-            <Column
-              name="name"
-              Cell={({ item: { name = '', decl = '' } = {} }: CellProps<unknown, { name?: string, decl?: string }>) => (
-                <div tw="pr-4"><Cells.Compound key={name} cellName={name} cellAdditionalInfo={decl} icon={<Icons.Function />} /></div>
-              )}
-              align="start"
-            />,
-            ...expandedColumns,
-            <Column
-              name="assocTestsCount"
-              label="Associated tests"
-              Cell={({
-                value = '',
-                item: { id = '' } = {},
-              }: CellProps<string, { id?: string; assocTestsCount?: number }>) => (
-                <Cells.Clickable
-                  data-test="coverage-details:associated-tests-count"
-                  disabled={!value}
-                >
-                  {value ? <Link to={getModalLink(id, 3)}>{value}</Link> : 'n/a'}
-                </Cells.Clickable>
-              )}
-            />,
-          ]}
-          expandedContentKey="name"
-          hasSecondLevelExpand
-        >
-          <Column
-            name="name"
-            label="Name"
-            Cell={({ value = '' }: CellProps<string, unknown>) => (
-              <NameCell
-                icon={<Icons.Package />}
-                value={value}
-                testContext="package"
-              />
-            )}
-            align="start"
-          />
-          <Column
-            name="coverage"
-            label={(
-              <div className="flex justify-end items-center w-full">
-                Coverage, %<Icons.Checkbox tw="ml-4 min-w-16px text-monochrome-default" width={16} height={16} />
-              </div>
-            )}
-            Cell={({ value = 0 }) => <CoverageCell value={value as number} showCoverageIcon={showCoverageIcon} />}
-          />
-          <Column name="totalMethodsCount" label="Methods total" testContext="total-methods-count" />
-          <Column name="coveredMethodsCount" label="Methods covered" testContext="covered-methods-count" />
-          <Column
-            name="assocTestsCount"
-            label="Associated tests"
-            Cell={({ value = '', item: { id = '' } = {} }: CellProps<string, { id?: string; assocTestsCount?: number }>) => (
-              <Cells.Clickable
-                data-test="coverage-details:associated-tests-count"
-                disabled={!value}
-              >
-                {value ? <Link to={getModalLink(id, 1)}>{value}</Link> : 'n/a'}
-              </Cells.Clickable>
-            )}
-          />
-        </ExpandableTable>
-        <ReTable
+        <Table
           columns={[...columns, ...infoColumns]}
           data={coverageByPackages}
           renderRowSubComponent={renderRowSubComponent}
