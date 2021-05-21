cfb71c7b26483db6bf45a61813bcbfcf967150a9
RomanAchilov <dakaktak95@gmail.com>
2021-05-21 13:45:07 +0300

    feat: add usePagination & search inside the table

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -13,45 +13,74 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+/* eslint-disable react/button-has-type */
 import React, { useMemo, useState } from 'react';
 import {
-  useTable, useExpanded, Column, useSortBy,
+  useTable, useExpanded, Column, useSortBy, usePagination,
 } from 'react-table';
 import { Icons } from '@drill4j/ui-kit';
 import { withErrorBoundary } from 'react-error-boundary';
 
-import { setSort, useTableActionsDispatch, useTableActionsState } from 'modules';
 import { TableErrorFallback } from 'components';
+import {
+  setSearch, setSort, useTableActionsDispatch, useTableActionsState,
+} from 'modules';
 import { Order } from 'types/sort';
 import tw, { styled } from 'twin.macro';
+import { SearchPanel } from 'components/search-panel';
 
 type CustomColumn = Column & { textAlign?: string; width?: string; }
 
 interface Props {
   columns: Array<CustomColumn>;
   data: Array<any>;
+  totalCount?: number;
+  filteredCount?: number;
+  placeholder?: string;
   renderRowSubComponent?: ({ row, rowProps }: any) => JSX.Element;
   stub?: React.ReactNode;
   isDefaulToggleSortBy?: boolean;
   columnsDependency?: Array<string | number | boolean | null | undefined>;
+  withSearchPanel?: boolean;
 }
 
 export const Table = withErrorBoundary(({
-  columns, data, renderRowSubComponent, stub = null, isDefaulToggleSortBy, columnsDependency = [],
+  columns,
+  data,
+  totalCount,
+  filteredCount = 0,
+  placeholder = 'Search value by name',
+  renderRowSubComponent,
+  stub = null,
+  isDefaulToggleSortBy,
+  columnsDependency = [],
+  withSearchPanel = true,
 }: Props) => {
   const {
+    page,
     getTableProps, getTableBodyProps, headerGroups, rows, prepareRow,
-  } = useTable(
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  }: any = useTable(
     {
       columns: useMemo(() => columns, [...columnsDependency]),
       data: useMemo(() => data, [data]),
     },
     useSortBy,
     useExpanded,
+    usePagination,
   );
 
   const dispatch = useTableActionsDispatch();
-  const { sort: [sort] } = useTableActionsState();
+  const { sort: [sort], search } = useTableActionsState();
+  const [searchQuery] = search;
 
   const [expandedRows, setExpandedRows] = useState<string[]>([]);
 
@@ -61,69 +90,129 @@ export const Table = withErrorBoundary(({
 
   return (
     <>
-      <table {...getTableProps()} tw="table-fixed w-full text-14 leading-16 text-monochrome-black">
-        <TableHead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()} tw="h-13 px-4">
-              {headerGroup.headers.map((column: any) => {
-                const active = column.id === sort?.field;
-                const defaulToggleSortBy = column.getSortByToggleProps().onClick;
+      {withSearchPanel && (
+        <div tw="mt-2">
+          <SearchPanel
+            onSearch={(searchValue) => dispatch(setSearch([{ value: searchValue, field: 'name', op: 'CONTAINS' }]))}
+            searchQuery={searchQuery?.value}
+            searchResult={filteredCount}
+            placeholder={placeholder}
+          >
+            Displaying {page.length} of {totalCount} packages
+          </SearchPanel>
+        </div>
+      )}
+      <div tw="overflow-x-auto">
+        <div style={{ minWidth: '1100px' }}>
+          <table {...getTableProps()} tw="table-fixed w-full text-14 leading-16 text-monochrome-black">
+            <TableHead>
+              {headerGroups.map((headerGroup: any) => (
+                <tr {...headerGroup.getHeaderGroupProps()} tw="h-13 px-4">
+                  {headerGroup.headers.map((column: any) => {
+                    const active = column.id === sort?.field;
+                    const defaulToggleSortBy = column.getSortByToggleProps().onClick;
+                    return (
+                      <TH
+                        {...column.getHeaderProps(column.getSortByToggleProps())}
+                        style={{ textAlign: column.textAlign || 'right', width: column.width }}
+                        onClick={isDefaulToggleSortBy
+                          ? defaulToggleSortBy
+                          : () => dispatch(setSort({ order: setOrder(sort?.order), field: column.id }))}
+                        data-test={`table-th-${column.id}`}
+                      >
+                        <div tw="relative inline-flex items-center cursor-pointer">
+                          {column.id !== 'expander' && (
+                            <SortArrow active={column.isSorted || active}>
+                              <Icons.SortingArrow rotate={column.isSortedDesc || (active && sort?.order === 'DESC') ? 0 : 180} />
+                            </SortArrow>
+                          )}
+                          {column.render('Header')}
+                        </div>
+                      </TH>
+
+                    );
+                  })}
+                </tr>
+              ))}
+            </TableHead>
+            <tbody {...getTableBodyProps()}>
+              {page.map((rawRow: any) => {
+                const row = { ...rawRow, isExpanded: expandedRows.includes(rawRow.original.id) };
+                prepareRow(row);
+                const rowProps = row.getRowProps();
                 return (
-                  <TH
-                    {...column.getHeaderProps(column.getSortByToggleProps())}
-                    style={{ textAlign: column.textAlign || 'right', width: column.width }}
-                    onClick={isDefaulToggleSortBy
-                      ? defaulToggleSortBy
-                      : () => dispatch(setSort({ order: setOrder(sort?.order), field: column.id }))}
-                    data-test={`table-th-${column.id}`}
-                  >
-                    <div tw="relative inline-flex items-center cursor-pointer">
-                      {column.id !== 'expander' && (
-                        <SortArrow active={column.isSorted || active}>
-                          <Icons.SortingArrow rotate={column.isSortedDesc || (active && sort?.order === 'DESC') ? 0 : 180} />
-                        </SortArrow>
-                      )}
-                      {column.render('Header')}
-                    </div>
-                  </TH>
+                  <React.Fragment key={row.original.id}>
+                    <TR {...rowProps} isExpanded={row.isExpanded}>
+                      {row.cells.map((cell: any) => (
+                        <td
+                          {...cell.getCellProps()}
+                          tw="first:px-4 last:px-4"
+                          style={{ textAlign: cell.column.textAlign || 'right' }}
+                          data-test={`td-${rowProps.key}-${cell.column.id}`}
 
-                );
-              })}
-            </tr>
-          ))}
-        </TableHead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((rawRow: any) => {
-            const row = { ...rawRow, isExpanded: expandedRows.includes(rawRow.original.id) };
-            prepareRow(row);
-            const rowProps = row.getRowProps();
-            return (
-              <React.Fragment key={row.original.id}>
-                <TR {...rowProps} isExpanded={row.isExpanded}>
-                  {row.cells.map((cell: any) => (
-                    <td
-                      {...cell.getCellProps()}
-                      tw="first:px-4 last:px-4"
-                      style={{ textAlign: cell.column.textAlign || 'right' }}
-                      data-test={`td-${rowProps.key}-${cell.column.id}`}
-
-                    >
-                      <div onClick={() => cell.column.id === 'expander' &&
+                        >
+                          <div onClick={() => cell.column.id === 'expander' &&
                         setExpandedRows(row.isExpanded
                           ? expandedRows.filter((id) => id !== row.original.id)
                           : [...expandedRows, row.original.id])}
-                      >
-                        {cell.render('Cell')}
-                      </div>
-                    </td>
-                  ))}
-                </TR>
-                {row.isExpanded && renderRowSubComponent?.({ row, rowProps })}
-              </React.Fragment>
-            );
-          })}
-        </tbody>
-      </table>
+                          >
+                            {cell.render('Cell')}
+                          </div>
+                        </td>
+                      ))}
+                    </TR>
+                    {row.isExpanded && renderRowSubComponent?.({ row, rowProps })}
+                  </React.Fragment>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page1 = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page1);
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(() => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
       {stub}
     </>
   );
